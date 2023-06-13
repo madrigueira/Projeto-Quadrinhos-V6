53cc43f5de55c862d79b07f9f5c81c295098bd6d
Matheus Negrini <mndias29@gmail.com>
2023-06-13 16:56:25 -0300

    deu 17h

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,10 @@ const App = () => {
             slug
             icon
             banner
+            series{
+              title
+              cover
+            }
           }
         }`
       );
--- src/pages/Character/index.jsx
+++ src/pages/Character/index.jsx
@@ -1,15 +1,27 @@
 import "./index.scss";
 
 const Character = ({ comics }) => {
+  const pogg =
+    "https://imgv2-1-f.scribdassets.com/img/document/465068966/original/be1247240e/1680792359?v=1";
   const bg = {
     backgroundImage:
-      "linear-gradient(transparent 0%, #1f1f1f 0%), url('" +
-      comics.banner +
-      "')",
+      "linear-gradient(transparent 0%, #1f1f1f 100%), url('" + pogg + "')",
   };
   return (
     <div className="character">
-      <div className="banner" style={bg}></div>
+      <div className="content">
+        <div className="banner" style={bg}></div>
+        <div className="grid">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
     </div>
   );
 };
