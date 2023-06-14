868fa1d6c026a4b8ee6527438a687e3090abc5f5
Matheus Negrini <mndias29@gmail.com>
2023-06-13 21:31:17 -0300

    antes tarde do q nunca

--- src/pages/Character/index.jsx
+++ src/pages/Character/index.jsx
@@ -1,25 +1,25 @@
 import "./index.scss";
+import { Link } from "react-router-dom";
 
 const Character = ({ comics }) => {
-  const pogg =
-    "https://imgv2-1-f.scribdassets.com/img/document/465068966/original/be1247240e/1680792359?v=1";
   const bg = {
     backgroundImage:
-      "linear-gradient(transparent 0%, #1f1f1f 100%), url('" + pogg + "')",
+      "linear-gradient(transparent 0%, #1f1f1f 100%), url('" +
+      comics.banner +
+      "')",
   };
   return (
     <div className="character">
       <div className="content">
         <div className="banner" style={bg}></div>
         <div className="grid">
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
+          {comics &&
+            comics.series.map(({ cover, title }) => (
+              <Link>
+                <img src={cover} />
+                <p>{title}</p>
+              </Link>
+            ))}
         </div>
       </div>
     </div>
